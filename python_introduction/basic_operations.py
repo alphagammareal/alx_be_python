fc79d4f21e4262bb9f794574a4ed5ee6eab91556
alphagammareal <sakyialpha2@gmail.com>
2025-09-08 11:05:27 +0000

    created basic operation file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+
+#subtraction
+subtraction = (number2 - number1)
+
+#addition
+addition = (number2 + number1)
+
+# Multiplication
+multiplication = (number2 * number1)
+
+print(f"Addition of 10 and 5 is {addition}") 
+print(f"Subtraction of 10 and 5 is {subtraction}")
+print(f"Multiplication of 10 and 5 is {multiplication}")
