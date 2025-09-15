74dcdc3daf98f3a54b71f889857eb1aece84a3f6
alphagammareal <sakyialpha2@gmail.com>
2025-09-15 20:33:00 +0000

    multiplication table added

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+# Prompt user for a number
+number = int(input("Enter a number to see its multiplication table: "))
+
+# generate and print the multiplication table
+
+
+for y in range(1, 11):
+    z = number * y
+    print(number, "*", y, "=", z )
