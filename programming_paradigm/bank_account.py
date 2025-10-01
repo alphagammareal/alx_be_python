b54c618e5a21cadd1f468ad76425273a65064784
ALPHA SAKYI <sakyialpha2@gmail.com>
2025-10-01 02:16:35 +0000

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,16 +7,16 @@ class BankAccount:
     # Deposit
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"Deposited: ${amount:.2f}")
+        print(f"Deposited: ${amount:.1f}")
 
     # Withdraw
     def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount:.2f}")
+            print(f"Withdrew: ${amount:.1f}")
         else:
             print("Insufficient funds.")
 
     # Display Balance
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.1f}")
